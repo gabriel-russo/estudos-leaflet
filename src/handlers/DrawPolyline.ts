406fe1861522691a8473d8ccca3c6627baa273ff
Gabriel Russo <gabrielrusso@protonmail.com>
2026-03-06 22:52:40 -0400

    Melhorias DrawPolyline

--- src/handlers/DrawPolyline.ts
+++ src/handlers/DrawPolyline.ts
@@ -1,10 +1,19 @@
-import {CircleMarker, DomEvent, Handler, LatLng, LeafletMouseEvent, Map, Point, Polyline} from 'leaflet';
+import {
+  CircleMarker,
+  DomEvent,
+  Handler,
+  LatLng,
+  LeafletMouseEvent,
+  Map,
+  Point,
+  Polyline,
+} from 'leaflet';
 
 export default class DrawPolyline extends Handler {
   protected _map: Map;
+  private _polyline?: Polyline;
   private _cursorMarker?: CircleMarker;
-  private _cursorPosition?: LatLng
-  private _polyline?: Polyline
+  private _cursorPosition?: LatLng;
   private _latlngs?: LatLng[];
 
   constructor(map: Map) {
@@ -12,32 +21,38 @@ export default class DrawPolyline extends Handler {
   }
 
   public addHooks() {
-    DomEvent.on(this._map.getContainer(), 'mousemove', this._updateCoords, this);
-    this._map.on('click', this._registerVertix, this);
-    this._map.on('dblclick', this._finishPolyline, this);
+    this._map.getContainer().style.cursor = 'cell';
+    DomEvent.on(this._map.getContainer(), 'mousemove', this.updateCoords, this);
+    this._map.on('click', this.registerVertex, this);
+    this._map.on('dblclick', this.finish, this);
   }
 
   public removeHooks() {
-    DomEvent.off(this._map.getContainer(), 'mousemove', this._updateCoords, this);
-    this._map.off('click', this._registerVertix, this);
-    this._map.off('dblclick', this._finishPolyline, this);
+    DomEvent.off(
+      this._map.getContainer(),
+      'mousemove',
+      this.updateCoords,
+      this
+    );
+    this._map.off('click', this.registerVertex, this);
+    this._map.off('dblclick', this.finish, this);
   }
 
-  private _updateCoords(e: Event & MouseEvent) {
-    if (!e) {
-      return;
-    }
-    this._cursorPosition = this._map.containerPointToLatLng(new Point(e.x, e.y));
+  private updateCoords(e: Event & MouseEvent) {
+    this._cursorPosition = this._map.containerPointToLatLng(
+      new Point(e.x, e.y)
+    );
     this.updateDraw();
-  }
-
-  private _savePolylineCoords() {
-    this._latlngs = this._polyline.getLatLngs() as LatLng[];
   }
 
   private updateDraw() {
     if (!this._cursorMarker) {
-      this._cursorMarker = new CircleMarker(this._cursorPosition, {radius: 5, color: 'red'});
+      this._cursorMarker = new CircleMarker(this._cursorPosition, {
+        radius: 2,
+        color: '#de6720',
+        opacity: 0.8,
+        interactive: false,
+      });
       this._cursorMarker.addTo(this._map);
       return;
     }
@@ -45,16 +60,15 @@ export default class DrawPolyline extends Handler {
     this._cursorMarker.setLatLng(this._cursorPosition);
 
     if (this._polyline) {
-
       if (!this._latlngs || !this._latlngs.length) {
-        this._savePolylineCoords();
+        this._latlngs = this._polyline.getLatLngs() as LatLng[];
       }
 
       this._polyline.setLatLngs([...this._latlngs, this._cursorPosition]);
     }
   }
 
-  private _registerVertix(e: LeafletMouseEvent) {
+  private registerVertex(e: LeafletMouseEvent) {
     if (!this._polyline) {
       this._polyline = new Polyline([e.latlng]);
       this._polyline.addTo(this._map);
@@ -64,7 +78,8 @@ export default class DrawPolyline extends Handler {
     this._latlngs.splice(0);
   }
 
-  private _finishPolyline() {
+  private finish() {
+    this._map.getContainer().style.cursor = 'grab';
     this._cursorMarker.removeFrom(this._map);
     delete this._cursorPosition;
     delete this._latlngs;
